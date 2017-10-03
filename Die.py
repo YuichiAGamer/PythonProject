45f56b71b925ebb4d9ff091368143ef11d25c1aa
YuichiAGamer <yw.philosophersstone@gmail.com>
2017-10-03 20:26:07 +0900

    Added __init__ and test code Die class.

--- Die.py
+++ Die.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+import random
 
-if __name__ == "__init__":
+
+class Die:
+    def __init__(self):
+        self.face = 6
+
+    def roll(self):
+        return random.randint(1, self.face)
+
+
+if __name__ == "__main__":
     print("This will be a Die class.")
+    die = Die()
+    print("The result was {}".format(die.roll()))
